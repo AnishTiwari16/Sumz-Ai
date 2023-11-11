b68f0b41ff3edef88c37af9e4d1cda83c531fba9
Anish Tiwari <anish.at.tiwari@gmail.com>
2023-11-11 20:04:39 +0530

    feat: added text to speech

--- components/heroSection.tsx
+++ components/heroSection.tsx
@@ -9,6 +9,10 @@ const HeroSection = () => {
     const [fetchSummaries, { isFetching, isSuccess }] =
         useLazyFetchSummariesQuery();
     const summaryData = useAppSelector((state) => state.summaryData);
+    const handleSpeak = (text: string) => {
+        const utterance = new SpeechSynthesisUtterance(text);
+        window.speechSynthesis.speak(utterance);
+    };
     return (
         <div className="flex items-center justify-center pt-16 flex-col">
             <h1 className="text-[32px] md:text-[40px] font-extrabold text-center leading-tight">
@@ -35,7 +39,7 @@ const HeroSection = () => {
                     <div role="status" className="flex items-center gap-3 pt-6">
                         <svg
                             aria-hidden="true"
-                            className="w-7 h-7 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                            className="w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                             viewBox="0 0 100 101"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg"
@@ -57,9 +61,24 @@ const HeroSection = () => {
             ) : (
                 isSuccess && (
                     <div className="pt-10 w-full md:w-[65%] lg:w-[40%]">
-                        <div className="font-semibold text-lg">
+                        <div className="font-semibold text-lg flex items-center gap-2">
                             Article{' '}
                             <span className="text-blue-600">Summary</span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                className="w-5 h-5 cursor-pointer"
+                                onClick={() => handleSpeak(summaryData)}
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
+                                />
+                            </svg>
                         </div>
                         <p className="pt-2 text-md">{summaryData}</p>
                     </div>
