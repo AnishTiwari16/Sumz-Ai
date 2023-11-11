857cd5efe0f6ce44f9d3ff04d3216b8264cd9e82
Anish Tiwari <anish.at.tiwari@gmail.com>
2023-11-11 19:52:31 +0530

    feat: added loading state and new api

--- components/InputComponent.tsx
+++ components/InputComponent.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Button } from './ui/button';
-import { useLazyFetchSummariesQuery } from '@/redux/api';
 import { useAppDispatch } from '@/redux/hooks';
 import { storeData } from '@/redux/slice';
-const InputComponent = () => {
-    const [fetchSummaries] = useLazyFetchSummariesQuery();
+import toast, { Toaster } from 'react-hot-toast';
+const InputComponent = ({ fetchSummaries }: { fetchSummaries: any }) => {
     const [searchedUrl, setSearchUrl] = useState('');
     const dispatch = useAppDispatch();
     const handleOnSubmit = async (e: React.FormEvent) => {
@@ -21,38 +20,41 @@ const InputComponent = () => {
                 dispatch(storeData(data.summary));
             }
         } catch (error) {
-            console.error(error);
+            toast.error('Invalid URL!!!');
         }
     };
     return (
-        <form
-            className="pt-12 w-full md:w-[65%] lg:w-[40%] relative"
-            onSubmit={handleOnSubmit}
-        >
-            <Input
-                placeholder="Enter a URL"
-                className="bg-[#FFFFFF]"
-                onChange={(e) => setSearchUrl(e.target.value)}
-            />
-            <Button
-                className="absolute top-[50px] right-0"
-                variant="outline"
-                size={'sm'}
+        <>
+            <Toaster position="top-center" reverseOrder={false} />
+            <form
+                className="pt-12 w-full md:w-[65%] lg:w-[40%] relative"
+                onSubmit={handleOnSubmit}
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-5 h-5"
+                <Input
+                    placeholder="Enter a URL"
+                    className="bg-[#FFFFFF]"
+                    onChange={(e) => setSearchUrl(e.target.value)}
+                />
+                <Button
+                    className="absolute top-[50px] right-0"
+                    variant="outline"
+                    size={'sm'}
                 >
-                    <path
-                        fillRule="evenodd"
-                        d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
-                        clipRule="evenodd"
-                    />
-                </svg>
-            </Button>
-        </form>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-5 h-5"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                </Button>
+            </form>
+        </>
     );
 };
 
